9e5845bfd2f5f5f8d2876025086af346a1cf140d
HeeseokMoon <bladder0808@google.com>
2020-11-23 20:24:05 +0900

    aa

--- R/bar.R
+++ R/bar.R
@@ -1,12 +1,14 @@
 bar<-function(data,
               title=NULL,subtitle=NULL,xtitle=NULL,ytitle=NULL,caption=NULL,...){
+  library(ggplot2)
+
   freqtable <- table(data)
   df <- as.data.frame.table(freqtable)
-  
-  
+
+
   p <- ggplot(df, aes(data, Freq))
-  p <- p + geom_bar(stat="identity", width = 0.5, fill="#008FD5") + 
-    theme_fivethirtyeight() + 
+  p <- p + geom_bar(stat="identity", width = 0.5, fill="#008FD5") +
+    theme_fivethirtyeight() +
     theme(axis.title = element_text(),
           axis.text.x = element_text(angle=65, vjust=0.6),
           legend.title = element_text(face = 4,size = 10),
@@ -16,7 +18,7 @@ bar<-function(data,
          x=xtitle,
          title=title,
          caption = caption)
-  
+
   return(p)
 }
 
